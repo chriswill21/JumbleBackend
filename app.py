727d85cdbcab30cf7e8b394771b0771adfcadcf2
Christien Spencer Williams <willch@fb.com>
2018-08-08 17:46:18 -0700

    still trying to deploy

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template
+import job_tagging_svm
+import compute_recommendation_model
+app = Flask(__name__)
+
+
+@app.route("/", methods = ['GET'])
+def hello():
+    return "Hello World!"
+    a = job_tagging_svm.predict_job_category()
+    print(a)
+    return str(a)
+
+@app.route("/tagging/<job_description>", methods = ['GET'])
+def job_tagging(job_description):
+    prediction = job_tagging_svm.predict_job_category(job_description)
+    print(prediction)
+    return str(prediction[0])
+
+@app.route("/recommending/<new_user_data_string>", methods = ['GET'])
+def job_recommendation(new_user_data_string):
+    # new_user_data = list(new_user_data)
+    new_user_data = []
+    num_builder = ""
+    data_point = []
+    start = False
+    for i in list(new_user_data_string):
+        if i == "(":
+            start = True
+        else:
+            if start:
+                if i != " " and i != "," and i != ")":
+                    num_builder += i
+                elif i == ",":
+                    data_point.append(int(num_builder))
+                    num_builder = ""
+                elif i == ")":
+
+                    data_point.append(int(num_builder))
+                    new_user_data.append(tuple(data_point))
+                    data_point = []
+                    num_builder = ""
+                    start = False
+
+
+    prediction = compute_recommendation_model.main(new_user_data)
+    return str(prediction)
+
+if __name__ == "__main__":
+    app.run()
