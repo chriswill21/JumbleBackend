23c114ff328c342f8d8855bad12c90f5f08fe114
Christien Spencer Williams <willch@fb.com>
2018-08-08 17:51:18 -0700

    trying to get up and running

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ import compute_recommendation_model
 app = Flask(__name__)
 
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
     return "Hello World!"
     a = job_tagging_svm.predict_job_category()
     print(a)
     return str(a)
 
-@app.route("/tagging/<job_description>", methods = ['GET'])
+@app.route("/tagging/<job_description>", methods=['GET', 'POST'])
 def job_tagging(job_description):
     prediction = job_tagging_svm.predict_job_category(job_description)
     print(prediction)
     return str(prediction[0])
 
-@app.route("/recommending/<new_user_data_string>", methods = ['GET'])
+@app.route("/recommending/<new_user_data_string>", methods=['GET','POST'])
 def job_recommendation(new_user_data_string):
     # new_user_data = list(new_user_data)
     new_user_data = []
